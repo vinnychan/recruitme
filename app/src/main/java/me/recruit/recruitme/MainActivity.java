c95b32de221b662b0008836699ed8c3b76e1c4e1
Alex Land <aland@sfu.ca>
2016-02-28 10:01:43 -0800

    Added LoginActivity and AuthUtil to do LinkedIn authentication (I hate LinkedIn)

--- app/src/main/java/me/recruit/recruitme/MainActivity.java
+++ app/src/main/java/me/recruit/recruitme/MainActivity.java
@@ -1,11 +1,13 @@
 package me.recruit.recruitme;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -15,8 +17,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.ArrayAdapter;
-import android.widget.TextView;
+
 
 import java.util.List;
 
@@ -33,6 +34,15 @@ public class MainActivity extends AppCompatActivity
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+        String token = preferences.getString(AuthUtil.TOKEN_PREFERENCE, "");
+        if (token.equals("")) {
+            Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+            startActivity(intent);
+            token = preferences.getString(AuthUtil.TOKEN_PREFERENCE, "");
+        }
+        Log.d("MainActivity", "Token: " + token);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -77,6 +87,8 @@ public class MainActivity extends AppCompatActivity
 
         CandidateAdapter candidateAdapter = new CandidateAdapter(candidateList, getApplicationContext(), cardCallback);
         recyclerView.setAdapter(candidateAdapter);
+
+
     }
 
     @Override
